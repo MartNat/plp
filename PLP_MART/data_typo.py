b7661c17c5dd63058f2587e9572a08d1ee2b951c
MartNat <martinanatasha5@gmail.com>
2024-02-28 20:44:40 +0300

    upload

--- PLP_MART/data_typo.py
+++ PLP_MART/data_typo.py
@@ -1,3 +1,4 @@
+#string
 a=100
 b=32
 c=3.14
@@ -7,12 +8,43 @@ c=3.14
 #print(a>b)
 d=12+2j
 #print(a+b+c+d)
+#initialize string
 plp_str= "Welcome to PLP Academy"
-print(plp_str)
+#print(plp_str)
 #slicing
-print(plp_str[0])
-print(plp_str[-1])
-print(plp_str[10:15])
+#print(plp_str[0])
+#print(plp_str[-1])
+#print(plp_str[10:15])
 student_str= ' Python Class'
-print(plp_str +student_str)
-print(student_str * 30)
+#concantenation
+#print(plp_str +student_str)
+#recur a string
+#print(student_str * 3)
+#list
+academy_ls = ['Jack', 30,3,4,2,5+7j,'Thika']
+student_ls = [30,5+7j]
+#print(academy_ls)
+#slice
+#print(academy_ls[0])
+#print(academy_ls[4:6])
+#concantenation
+#print(academy_ls + student_ls)
+#recurring
+#print(academy_ls * 5)
+#tuple
+tuple = ('abcd', 786 , 2.23, 'john' , 70.2)
+tinytuple = 123, 5+6j, 'john'
+#print(tuple)
+#print(tuple[2:3])
+#print(tuple + tinytuple)
+#print(tinytuple * 3)
+#dictionary
+dict = {}
+dict['one'] = "this is one"
+dict[2] = "this is two"
+tinydict = {'name': 'john','code':6734, 'dept':['Sales']}
+print(dict)
+print(dict['one'])
+print(tinydict)
+print(tinydict.keys())
+print(tinydict.values())
